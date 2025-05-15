66a9c566b6b8289123963e2dfd6a8f0832cae57f
auxiliarymaxima <93989258+auxiliarymaxima@users.noreply.github.com>
2025-05-15 12:37:06 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,14 @@
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  self.skipWaiting(); // Force it to activate right away
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    self.clients.claim() // Start controlling pages immediately
+  );
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => {
+      client.postMessage("Service worker activated and in control!");
+    });
+  });
 });
